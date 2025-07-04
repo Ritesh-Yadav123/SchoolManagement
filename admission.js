b17c81004d11281988b013e1c3f69d36a58608d2
Ritesh <riteshyadav825410@gmail.com>
2025-07-05 02:22:07 +0530

    completed admission

--- admission.js
+++ admission.js
@@ -1,14 +1,63 @@
-const express=require('express');
-const path=require('path');
-const db=require('./db');
-const router=express.Router();
+const express = require('express');
+const path = require('path');
+const db = require('./db');
+const router = express.Router();
 
-const pathname=path.join(__dirname,"public");
-// router.use(express.static(pathname));
+const pathname = path.join(__dirname, "public");
 
-router.get("/",(req,res)=>{
-    res.sendFile(path.join(pathname,"admission.html"));
-})
+// Parse incoming data
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+// Route: Serve admission form
+router.get("/", (req, res) => {
+    res.sendFile(path.join(pathname, "admission.html"));
+});
+
+// Connect to MySQL
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Admission table connected to MySQL database");
+});
+
+// Route: Handle form submission
+router.post('/', (req, res) => {
+    const {
+        full_name,
+        gender,
+        date_of_birth,
+        class: studentClass,
+        section,
+        contact_number
+    } = req.body;
+
+    const sql = `
+  INSERT INTO admission 
+  (full_name, gender, date_of_birth, \`class\`, section, contact_number)
+  VALUES (?, ?, ?, ?, ?, ?)
+`;
 
 
-module.exports=router;
+    db.query(sql, [
+        full_name,
+        gender,
+        date_of_birth,
+        studentClass,
+        section,
+        contact_number
+    ], (err, result) => {
+        if (err) {
+            console.error("Error while inserting the data:", err);  // 💥 Replaced window.alert
+            return res.status(500).send('Something went wrong!');
+        }
+        console.log('Admission Form Submitted:', result);
+        res.send("Admission Taken");
+    });
+});
+
+// Custom route for testing
+router.get("/ritesh", (req, res) => {
+    res.send("You are great Ritesh");
+});
+
+module.exports = router;
