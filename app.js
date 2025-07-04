59be1cd60d324cde08c0acbfcc26a1c6d0ba95af
Ritesh <riteshyadav825410@gmail.com>
2025-07-04 05:55:27 +0530

    solve the issue in admission.html

--- app.js
+++ app.js
@@ -37,51 +37,6 @@ app.get("/dashboard", (req, res) => {
 });
 
 
-app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(pathname, "classes.html"));
-});
-
-
-
-
-// app.post("/login", (req, res, next) => {
-//   const { email, password } = req.body;
-
-//   if (!email || !password) {
-//     return res
-//       .status(400)
-//       .json({ success: false, message: "Email and password are required" });
-//   }
-
-//   const sanitizedEmail = String(email).trim();
-//   const sanitizedPassword = String(password).trim();
-
-//   const query =
-//     "SELECT * FROM admin WHERE (email = ? OR user = ?) AND password = ?";
-//   db.query(
-//     query,
-//     [sanitizedEmail, sanitizedEmail, sanitizedPassword],
-//     (err, results) => {
-//       if (err) {
-//         console.error("Database query error:", err);
-//         return res
-//           .status(500)
-//           .json({ success: false, message: "Internal server error" });
-//       }
-
-//       if (results.length > 0) {
-//         res.redirect("/dashboard?message=Login%20successful");
-//       } else {
-//         res
-//           .status(401)
-//           .json({ success: false, message: "Invalid email or password" });
-//       }
-//     }
-//   );
-// });
-
-
-
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
@@ -109,15 +64,13 @@ function isAuthenticated(req, res, next) {
   if (req.session.isLoggedIn) {
     return next();
   }
-  return res.status(401).send("Unauthorized access Ask Ritesh For clearification");
+  return res.status(401).sendFile(path.join(pathname, "error.html"));
 }
 
 // 👇 Protect /student routes
 app.use("/student", isAuthenticated, studentRoutes);
 
 app.use("/admission", isAuthenticated, admissionRoutes);
-
-
 
 
 const PORT = 5000;
